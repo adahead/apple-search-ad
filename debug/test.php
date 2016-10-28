201b2144b82b64ee931c898386bf621e2d4ac7e2
Sergey Kubrey <kubrey@gmail.com>
2016-10-28 10:56:34 +0300

    test examples addes

---
+++ debug/test.php
@@ -0,0 +1,67 @@
+<?php
+/**
+ * @author Sergey Kubrey <kubrey.work@gmail.com>
+ *
+ */
+
+include_once '../src/searchad/BaseApi.php';
+include_once '../src/searchad/ApiRequest.php';
+include_once '../src/searchad/reports/ReportingRequest.php';
+
+include_once '../src/searchad/selector/Conditions.php';
+include_once '../src/searchad/selector/Selector.php';
+
+$rep = new \searchad\reports\ReportingRequest();
+
+$repParams = '{
+    "startTime": "2016-01-01T00:00:00.000",
+    "endTime": "2017-10-01T00:00:00.000",
+    "selector": {
+    	"orderBy":[{"field":"campaignId","sortOrder":"DESCENDING"}]
+    },
+    "granularity":"MONTHLY"
+}';
+
+$rep->loadCertificates(__DIR__ . '/test.pem', __DIR__ . '/test.key')
+    ->queryReports($repParams);
+
+$cond = new \searchad\selector\Conditions();
+$cond->addCondition("countryCode", \searchad\selector\Conditions::OPERATOR_IN, ["US"]);
+//$cond->addCondition("modificationTime", \searchad\selector\Conditions::OPERATOR_LESS_THAN, ["2016-10-21T0:0:0.00"]);
+
+$res = $cond->getConditions();
+
+$sel = new \searchad\selector\Selector();
+
+$selData = $sel->orderBy("modificationTime")
+    ->selectFields(["taps","impressions"])
+    ->setLimit(10)
+    ->setOffset(0)
+    ->setConditions($res)
+    ->getSelector(true);
+
+$repParams1 = '{
+    "startTime": "2016-01-01T00:00:00.000",
+    "endTime": "2017-10-01T00:00:00.000",
+    "selector": '.$selData.',
+    "granularity":"MONTHLY"
+}';
+$rep->queryReports($repParams1);
+
+var_dump(json_decode($rep->getRawResponse(),true));
+
+
+//var_dump($rep->getRawResponse(), $rep->getCurlInfo());
+
+/*
+ *
+ *
+ *  curl --cert test.pem --key test.key -d '{
+    "startTime": "2016-01-01T00:00:00.000",
+    "endTime": "2017-10-01T00:00:00.000",
+    "selector": {
+    "orderBy":[{"field":"campaignId","sortOrder":"DESCENDING"}]
+    },
+    "granularity":"HOURLY"
+}' -H "Content-type: application/json" -X POST "https://api.searchads.apple.com/api/v1/reports/campaigns/"
+ */
